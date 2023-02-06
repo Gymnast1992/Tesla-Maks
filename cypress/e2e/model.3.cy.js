444be5db8af452be2e419da9b44a6da5821b4a0c
Maksym Semiankiv <1992maksym1992@gmail.com>
2023-02-06 09:51:04 +0900

    pom

--- cypress/e2e/model.3.cy.js
+++ cypress/e2e/model.3.cy.js
@@ -1,23 +1,25 @@
+import Model3 from "../fixtures/pom/model.3.page"
+
 describe("Model 3", () => {
   beforeEach(() => {
     cy.visit("/model3");
   });
 
   it("Should verify the price of the Model 3 Rear-Wheel Drive", () => {
-    cy.contains("Order Now").click({ force: true });
-    cy.get('[for="$MT322-Model 3"]').should(
+    Model3.buttonOrderNow.click({ force: true });
+    Model3.fieldRearWheelDrive.should(
       "have.text",
       "Rear-Wheel DriveModel 3$43,490"
     );
   });
 
   it("Should navigate to the Model 3 payment page", () => {
-    cy.contains("Order Now").click({ force: true });
-    cy.contains("Continue to Payment").click();
-    cy.get('[class="cf-h2-title tds-text--center"]').should(
+    Model3.buttonOrderNow.click({ force: true });
+    Model3.buttinContinueToPayment.click();
+    Model3.textYourModel3.should(
       "have.text",
       "Your Model 3"
     );
-    cy.get('[value="CREDITCARD"]').should("have.text", "Order with Card");
+    Model3.buttonOrderWithCard.should("have.text", "Order with Card");
   });
 });
---
+++ cypress/fixtures/pom/model.3.page.js
@@ -0,0 +1,9 @@
+class Model3 {
+    get buttonOrderNow() {return cy.contains("Order Now")}
+    get buttinContinueToPayment() {return cy.contains("Continue to Payment")}
+    get textYourModel3() {return cy.get('[class="cf-h2-title tds-text--center"]')}
+    get buttonOrderWithCard() {return cy.get('[value="CREDITCARD"]')}
+    get fieldRearWheelDrive() {return cy.get('[for="$MT322-Model 3"]')}
+}
+
+export default new Model3()
