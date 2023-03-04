220e7861dfb4b19d1b1068d2d1be48d6429780a2
Maksym Semiankiv <1992maksym1992@gmail.com>
2023-03-04 20:36:16 +0900

    new TC Contact

--- cypress/e2e/homepage/homepage.cy.js
+++ cypress/e2e/homepage/homepage.cy.js
@@ -1,4 +1,5 @@
-import Homepage from "../../fixtures/pom/homepage/homepage.page"
+import Homepage from "../../fixtures/pom/homepage/homepage.page";
+import Contacts from "../../fixtures/pom/homepage/contacts.page";
 
 describe("Homepage", () => {
   beforeEach(() => {
@@ -9,5 +10,17 @@ describe("Homepage", () => {
     Homepage.buttonMenu.should("be.visible");
     Homepage.buttonCustomerOrder.should("be.visible");
     Homepage.buttonDemoDrive.should("be.visible");
+  });
+
+  it("Should navigate to the Contact page", () => {
+    Homepage.fullPage.scrollTo('bottom');
+    Homepage.buttonContact.click({force: true});
+    Contacts.h1.should('contain.text', 'Contact');
+    Contacts.sectionTeslaHQTexas
+      .should('contain.text', 'Tesla Headquarters - Gigafactory Texas');
+    Contacts.sectionTeslaHQTexas
+      .should('contain.text', '1 Tesla Road')
+    Contacts.sectionTeslaHQTexas
+      .should('contain.text', 'Austin, TX 78725')    
   });
 });
